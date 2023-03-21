769f83c79a87c3e8db779ae58b7fc231ade5c325
Siarhei Konau <skonau@scnsoft.com>
2023-03-21 10:04:00 +0300

    Showed authors in the main page

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -8,6 +8,7 @@ use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Author;
+use App\Repository\AuthorRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 //#[Autoconfigure(calls: ['setMyClass' => '@doctrine'])]
@@ -19,23 +20,35 @@ class AuthorController extends AbstractController
 //        $this->doctrine = $doctrine;
 //    }
 
-    #[Route('/author', name: 'app_author')]
+    #[Route('/author', name: 'add_author')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-        $author = new Author();
-        $author->setName('Tolkien');
-        $author->setAge(43);
-        $author->setGenre('Fantasy');
 
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($author);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
-
-        return new Response('Saved new product with id '.$author->getId());
+//        $author = new Author();
+//        $author->setName('Tolkien');
+//        $author->setAge(43);
+//        $author->setGenre('Fantasy');
+//
+//        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+//        $entityManager->persist($author);
+//
+//        // actually executes the queries (i.e. the INSERT query)
+//        $entityManager->flush();
+//
 //        return $this->render('author/index.html.twig', [
 //            'controller_name' => 'AuthorController',
 //        ]);
+    }
+
+    #[Route('/author/all', name: 'app_author')]
+    public function show(EntityManagerInterface $entityManager): Response
+    {
+        $repository = $entityManager->getRepository(Author::class);
+        $author = $repository->findAll();
+
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+            'authors' => $author
+        ]);
     }
 }
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Author;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $repository = $entityManager->getRepository(Author::class);
+        $authors = $repository->findAll();
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'authors' => $authors
         ]);
+
     }
 }
