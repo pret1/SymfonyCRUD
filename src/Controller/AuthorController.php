8fb6d90c79a7e19fab6effa2a890c008b74359d7
Siarhei Konau <skonau@scnsoft.com>
2023-03-23 15:34:41 +0300

    Complete CRUD app1.

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,48 +14,25 @@ use App\Form\AuthorType;
 use App\Repository\AuthorRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
-//#[Autoconfigure(calls: ['setMyClass' => '@doctrine'])]
 class AuthorController extends AbstractController
 {
-//    private $doctrine;
-//    public function setMyClass(ManagerRegistry $doctrine)
-//    {
-//        $this->doctrine = $doctrine;
-//    }
-
     #[Route('/author', name: 'add_author')]
     public function add(Request $request, EntityManagerInterface $entityManager): Response
     {
         $author = new Author();
         $form = $this->createForm(AuthorType::class, $author);
-
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $author = $form->getData();
             $entityManager->persist($author);
             $entityManager->flush();
 
-            $this->addFlash('success', 'Create success');
+            $this->addFlash('success', 'Success');
             return $this->redirectToRoute('app_main');
         }
         return $this->render('author/add.html.twig', [
             'form' => $form,
         ]);
-
-//        $author = new Author();
-//        $author->setName('Tolkien');
-//        $author->setAge(43);
-//        $author->setGenre('Fantasy');
-//
-//        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-//        $entityManager->persist($author);
-//
-//        // actually executes the queries (i.e. the INSERT query)
-//        $entityManager->flush();
-//
-//        return $this->render('author/index.html.twig', [
-//            'controller_name' => 'AuthorController',
-//        ]);
     }
 
     #[Route('/author/{id}', name: 'show_author')]
@@ -74,14 +52,13 @@ class AuthorController extends AbstractController
         $repository = $entityManager->getRepository(Author::class);
         $author = $repository->find($id);
         $form = $this->createForm(AuthorType::class, $author);
-
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $author = $form->getData();
             $entityManager->persist($author);
             $entityManager->flush();
 
-            $this->addFlash('success', 'Success update');
+            $this->addFlash('success', 'Success');
             return $this->redirectToRoute('app_main');
         }
         return $this->render('author/edit.html.twig', [
@@ -100,4 +77,6 @@ class AuthorController extends AbstractController
         $this->addFlash('success', 'Success delete');
         return $this->redirectToRoute('app_main');
     }
+
+
 }
